a40b27af1e0fb7bf88c946d8e856ad706f1b614c
Tatevik8 <tvk25@bk.ru>
2023-03-11 12:47:03 +0300

    Program writes the arithmetic mean of the elements of each column of a two-dimensional array of integers.

---
+++ domzad3/Program.cs
@@ -0,0 +1,63 @@
+﻿// **Задача 52.** Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 
+// 25 9 2 
+// 38 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+void SredneArifmetStolbcovMatrix(int[,] matr)
+{
+    Console.Write("Среднее арифметическое элементов в каждом столбце равно следующим значениям: ");
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i, j];
+        }
+        Console.Write(Math.Round((double)sum / matr.GetLength(0), 1) + "; ");
+    }
+    Console.WriteLine();
+}
+
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+SredneArifmetStolbcovMatrix(myMatrix);
